48869884f3f79e8842a22535c268bfcab70aaf2d
Suyash Rai <thesuyashrai@gmail.com>
2025-08-06 02:08:52 +0530

    Increase timeout maximum for Python code execution to 2000

--- routes/v1/code/execute/execute_python.py
+++ routes/v1/code/execute/execute_python.py
@@ -35,7 +35,7 @@ logger = logging.getLogger(__name__)
     "type": "object",
     "properties": {
         "code": {"type": "string"},
-        "timeout": {"type": "integer", "minimum": 1, "maximum": 300},
+        "timeout": {"type": "integer", "minimum": 1, "maximum": 2000},
         "webhook_url": {"type": "string", "format": "uri"},
         "id": {"type": "string"}
     },
